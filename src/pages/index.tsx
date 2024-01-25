e719912443f6370d006eb576702a72e1bdd4f32b
Mishragini <mishragini11.com>
2024-01-25 08:30:16 +0530

    add responsive Side Navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,24 @@ import { Inter } from "next/font/google";
 import VedioCard from "@/components/VedioCard";
 import VedioGrid from "@/components/VedioGrid";
 import Appbar from "@/components/Appbar";
+import SideNavbar from "@/components/SideNavbar";
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from 'recoil';
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-   <div>
+   <RecoilRoot>
+    <SideNavbar />
+
     <Appbar />
     <VedioGrid />
-   
-   </div>
+   </RecoilRoot>
   );
 }
---
+++ src/store/atom.tsx
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const sideNavbarState=atom({
+    key:'sideNavbarState',
+    default:false
+})
